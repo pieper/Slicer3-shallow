f77579857f5e2db058645047998d8380c5f41e61
pieper <pieper@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-09-22 19:04:29 +0000

    COMP: switch patch version to 3.6.2-beta
    git-svn-id: http://svn.slicer.org/Slicer3/trunk@15042 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/getbuildtest.tcl
+++ Scripts/getbuildtest.tcl
@@ -431,7 +431,7 @@ if { $::GETBUILDTEST(version-patch) == "" } {
 }
 
 # set the binary filename root
-set ::GETBUILDTEST(binary-filename) "Slicer3-3.6.1-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
+set ::GETBUILDTEST(binary-filename) "Slicer3-3.6.2-beta-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
 if {$::GETBUILDTEST(verbose)} {
     puts "CPack will use $::::GETBUILDTEST(binary-filename)"
 }
