acb01bd73d57fe5f3d2d39669548e396c3c83dee
domibel <domibel@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-11-11 21:00:34 +0000

    ENH: Change parameter for BRAINSFit: Increase the numberOfSamples, also increase the splineGridSize
    git-svn-id: http://svn.slicer.org/Slicer3/trunk@15474 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/EMSegment/Tasks/GenericTask.tcl
+++ Modules/EMSegment/Tasks/GenericTask.tcl
@@ -910,12 +910,12 @@ namespace eval EMSegmenterPreProcessingTcl {
         }
 
         if {$fastFlag} {
-            set CMD "$CMD --numberOfSamples 1000"
-        } else {
             set CMD "$CMD --numberOfSamples 10000"
+        } else {
+            set CMD "$CMD --numberOfSamples 100000"
         }
 
-        set CMD "$CMD --numberOfIterations 1500 --minimumStepLength 0.005 --translationScale 1000.0 --reproportionScale 1.0 --skewScale 1.0 --splineGridSize 14,10,12 --fixedVolumeTimeIndex 0 --movingVolumeTimeIndex 0 --medianFilterSize 0,0,0 --numberOfHistogramBins 50 --numberOfMatchPoints 10 --useCachingOfBSplineWeightsMode ON --useExplicitPDFDerivativesMode AUTO --relaxationFactor 0.5 --maximumStepLength 0.2 --failureExitCode -1 --debugNumberOfThreads -1 --debugLevel 0 --costFunctionConvergenceFactor 1e+9 --projectedGradientTolerance 1e-5 --costMetric MMI"
+        set CMD "$CMD --numberOfIterations 1500 --minimumStepLength 0.005 --translationScale 1000.0 --reproportionScale 1.0 --skewScale 1.0 --splineGridSize 28,20,24 --fixedVolumeTimeIndex 0 --movingVolumeTimeIndex 0 --medianFilterSize 0,0,0 --numberOfHistogramBins 50 --numberOfMatchPoints 10 --useCachingOfBSplineWeightsMode ON --useExplicitPDFDerivativesMode AUTO --relaxationFactor 0.5 --maximumStepLength 0.2 --failureExitCode -1 --debugNumberOfThreads -1 --debugLevel 0 --costFunctionConvergenceFactor 1e+9 --projectedGradientTolerance 1e-5 --costMetric MMI"
 
         $LOGIC PrintText "TCL: Executing $CMD"
         catch { eval exec $CMD } errmsg
